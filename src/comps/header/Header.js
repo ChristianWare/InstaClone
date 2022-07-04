74c8b5be3a0c0bc7e77c477d771baf3556075a8b
ChristianWare <c.p.ware@hotmail.com>
2022-07-04 00:34:27 -0700

    Logo Images and Layout Comp

--- src/comps/header/Header.js
+++ src/comps/header/Header.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import styles from "./Header.module.css";
+import insta from "../../../public/images/instaLogo.png";
 
 function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.content}>
-        <div className={styles.left}>Left</div>
+        <div className={styles.left}>
+          <Image src={insta} className={styles.logo} width={140} height={50} />
+        </div>
         <div className={styles.middle}>Middle</div>
         <div className={styles.right}>Right</div>
       </div>
---
+++ src/comps/layout/Layout.js
@@ -0,0 +1,11 @@
+import styles from "./Layout.module.css";
+
+function Layout({ children }) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.layout}>{children}</div>
+    </div>
+  );
+}
+
+export default Layout;
